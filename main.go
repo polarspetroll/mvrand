a4b43b5b9123c85015d1870ab85611a4a429b205
arian firoozfar <polarspetroll@gmail.com>
2021-05-04 17:00:09 +0430

    added current directory rename function

--- main.go
+++ main.go
@@ -1,28 +1,73 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "log"
-  "strconv"
-  "crypto/rand"
-  "encoding/hex"
-  "path/filepath"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+  "io/ioutil"
 )
 
 func main() {
-  filename, length := InputCheck()
-  if length == -1 {
-    length = 5
+	filename, length := InputCheck()
+	if length == -1 {
+		length = 5
+	}
+  if filename == "." {
+		RenameAll(length)
+    return
+	}
+	outname := RandName(length)
+  ex := filepath.Ext(filename)
+  fmt.Println(filename+ " =>" + outname + ex)
+	err := os.Rename(filename, outname+ex)
+	CheckErr(err)
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func RenameAll(length int) {
+  files, err := ioutil.ReadDir(".")
+  CheckErr(err)
+  for _, f := range files {
+    outname := RandName(length)
+    ex := filepath.Ext(f.Name())
+    fmt.Println(f.Name() + "=> " + outname+ex)
+    err = os.Rename(f.Name(), outname+ex)
   }
-  rbyte := make([]byte, length)
-  _, err := rand.Read(rbyte)
-  CheckErr(err)
-  outname := hex.EncodeToString(rbyte)
-  err = os.Rename(filename, outname + filepath.Ext(filename))
-  CheckErr(err)
 }
 
+func RandName(length int) string {
+	rbyte := make([]byte, length)
+	_, err := rand.Read(rbyte)
+	CheckErr(err)
+	return hex.EncodeToString(rbyte)
+}
+
+func InputCheck() (filename string, length int) {
+	if len(os.Args) < 2 || os.Args[1] == "-h" {
+		fmt.Println(banner)
+		os.Exit(0)
+	}
+	filename = os.Args[1]
+	if len(os.Args) > 2 && os.Args[2] == "-l" {
+		num := os.Args[3]
+		length, err := strconv.Atoi(num)
+		CheckErr(err)
+		length = length / 2
+		return filename, length
+	}
+	return filename, -1
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var banner string = `
 Usage : mvrand [file name] [options]
 
@@ -30,27 +75,3 @@ Options :
 -h help
 -l length of the random file name (default: 10)
 `
-
-
-func InputCheck() (filename string, length int){
-  if len(os.Args) < 2 || os.Args[1] == "-h" {
-    fmt.Println(banner)
-    os.Exit(0)
-  }
-  filename = os.Args[1]
-  if len(os.Args) > 2 && os.Args[2] == "-l" {
-    num := os.Args[3]
-    length, err := strconv.Atoi(num)
-    CheckErr(err)
-    length = length / 2
-    return filename, length
-  }
-  return filename, -1
-}
-
-
-func CheckErr(err error) {
-  if err != nil {
-    log.Fatal(err)
-  }
-}
